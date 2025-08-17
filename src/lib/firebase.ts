5c077ab28cc598e55f9e99062b6180237be7664b
kokiyum adskaro <mgreyshoe@gmail.com>
2025-08-17 18:39:18 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -5,13 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "goodluck-5pbn8",
-  "appId": "1:966790246062:web:b912e60cdde4c3517a873f",
-  "storageBucket": "goodluck-5pbn8.firebasestorage.app",
-  "apiKey": "AIzaSyBoDJpiQSAoqJoQcxnR-6TYK4pAMpJKJ8I",
-  "authDomain": "goodluck-5pbn8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "966790246062"
+  apiKey: "AIzaSyBoDJpiQSAoqJoQcxnR-6TYK4pAMpJKJ8I",
+  authDomain: "goodluck-5pbn8.firebaseapp.com",
+  projectId: "goodluck-5pbn8",
+  storageBucket: "goodluck-5pbn8.firebasestorage.app",
+  messagingSenderId: "966790246062",
+  appId: "1:966790246062:web:13e5c9c60ae7cafe7a873f"
 };
 
 // Initialize Firebase
